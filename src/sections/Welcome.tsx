f0fb48e71d20c357d308d82eb3053dac67396929
Konrad <konrad.wojtylo.9@gmail.com>
2025-01-25 20:11:39 +0100

    Added welcome section and made minor styles improvements

--- src/sections/Welcome.tsx
+++ src/sections/Welcome.tsx
@@ -1,21 +1,25 @@
 import { Link } from 'react-router-dom';
 import { scrollToTop } from '../utils/scrollToTopUtils';
+import { FaBook, FaPencilAlt, FaPenFancy } from 'react-icons/fa';
 
 const Welcome: React.FC = () => {
 	return (
-		<section style={{ minHeight: '100vh' }}>
-			<div>
-				<h1>Aplikacja do nauki</h1>
+		<section className='welcome'>
+			<div className='welcome__text-container'>
+				<h1>Learnica</h1>
+				<p>Lekcje na wyciągnięcie ręki..</p>
+				<div className='welcome__btn-box'>
+					<Link to='/logowanie' onClick={scrollToTop}>
+						Logowanie
+					</Link>
+				</div>
+				<FaBook className='welcome__text-container-icon welcome__text-container-icon--first' fontSize={70} />
+				<FaPencilAlt className='welcome__text-container-icon welcome__text-container-icon--second' fontSize={66} />
+				<FaPenFancy className='welcome__text-container-icon welcome__text-container-icon--third' fontSize={54} />
+				<FaBook className='welcome__text-container-icon welcome__text-container-icon--fourth' fontSize={62} />
+				<FaPencilAlt className='welcome__text-container-icon welcome__text-container-icon--fifth' fontSize={80} />
 			</div>
-			<div>
-				<Link to='/logowanie' onClick={scrollToTop}>
-					Logowanie
-				</Link>
-				<br />
-				<Link to='/rejestracja' onClick={scrollToTop}>
-					Rejestracja
-				</Link>
-			</div>
+			<div className='welcome__image-container'></div>
 		</section>
 	);
 };
