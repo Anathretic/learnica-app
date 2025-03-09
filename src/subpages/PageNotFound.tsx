d391709e0c956338e9f39aa270a8293cb6699302
Konrad <konrad.wojtylo.9@gmail.com>
2025-03-09 23:46:29 +0100

    Added notfound subpage and optimized blogData helper

--- src/subpages/PageNotFound.tsx
+++ src/subpages/PageNotFound.tsx
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom';
+import { scrollToTop } from '../utils/scrollToTopUtils';
+
+import NotFoundImage from '../images/notfound-subpage/notfound-image.png';
+
 const PageNotFound: React.FC = () => {
 	return (
-		<section>
-			<div>
-				<h1>Ups! Nie znaleźliśmy tej strony!</h1>
+		<section className='notfound'>
+			<div className='notfound__container'>
+				<img className='notfound__img' src={NotFoundImage} alt='Obrazek informujący o braku strony' />
+				<h1 className='notfound__credits'>Ups! Strony nie znaleziono..</h1>
+				<Link to='/' className='notfound__btn' onClick={scrollToTop} aria-label='Powrót na stronę główną'>
+					Powrót
+				</Link>
 			</div>
 		</section>
 	);
